83d2953aa42ffdd3eced99d2b5c97880adf281bb
Jon Estil Krågebakk <jonekra@ntnu.no>
2025-02-12 14:42:46 +0100

    pvtu support added for verts (not for mesh yet), geometry node added to blend file to convert mesh/verts to points and assignmed water material

--- README.md
+++ README.md
@@ -1,27 +1,57 @@
 # AquaRenderer
-This is a blender-project that loads NHFLOW simulations from .vtu files into blender so that the simnulation can be rendered in the blenders render engines with blenders flexible artistic tools, that your simulation deserves  
+This is a blender-project that loads NHFLOW simulations from .vtu files - such as the output from DiveMesh and REEF3D - into blender so that the simnulation can be rendered in the blenders render engines with blenders flexible artistic tools, that your simulation deserves  
 
 # Pre-requirements:
 - Blender
-- Python  
+- Python 
+- acces to terminal [Duh]
 
 # Optional requiremenmts:
 - VS Code
 
 # HOW 2 - STEP BY STEP
 1. Download repo: 
-    - Option 1: git clone <https://link.git> 
+    * <> Code 
+    - Option 1: git clone https://github.com/estilmk47/AquaRenderer.git 
     - Option 2: Downlaod zip
 
-2. Setup the python environment:
+2. Setup the IDE and python environment:
     - Open a terminal in the main repo 
-    - Use the terminal cmd: python -m venv <name_of_your_viritual_environment> (Recomended to use '.venv' or 'venv')
+    - Create the environment:
+        Make sure to use the same python version as your blender install
+        - Use the terminal cmd: python -m venv <name_of_your_viritual_environment> (Recomended to use '.venv' or 'venv')
+        - Use a similar terminal cmd: & "C:\Program Files\Blender Foundation\Blender 4.2\4.2\python\bin\python.exe" -m venv venv (This ensures packages you later install with pip is compatible with the python that is ran in blender)
     - Change the venv variable in the sccript.py to whatever you chose to name your environment (If you named it .venv you do not need to change this variable)
     - activate the viritual environment:
         - WINDOWS: <name_of_your_viritual_environment>/Script/activate
-        - Other operating systems: "You know better then me and can probably find it out"
+        - Other operating systems: "You know better then me, or can probably find it out faster then me"
 
 3. Install python requirements:
-    - 
+    - NB: Ensure and resolve that the python environment is of the same version that your blender version is runnig [Probably ]   
+    - When the venv is activated run:
+        - pip install -r requirements.txt
 
-Technically you only need to install the 
+4. Run the script from blender:
+    - Open blender.
+    - Navigate to the scripting window.
+    - Delete the current scripts path as it is not linked to the script.py file on your computer
+    - Open a new Script/Text 'script.py' [and rename it - good practice]
+    - OPTIONAL:
+        - Change the all the lines commented with TODO in the *if __name__ == '__main__':* part of the script if you do not want to run the example. (The very bottom of almost all python scripts that has it) 
+        - Click on Window > Toggle System Console [This tab is very convenient for debugging, you may print directly to this terminal from your python scripts]
+    - Run the script (Play button in scripting tab, *or*, Alt+P for hotkey-users)
+
+Technically you only need to install the python packages after *sys.path.append(pyhton_env_path) [16]* as the other ones are either a part of python core or the python packages that came with Blender.
+If you don't know which libraries these are [but you want to know], may I suggest looking in the path similar to C:\Program Files\Blender Foundation\Blender 4.2\4.2\python\lib\site-packages on your computer.
+
+
+# TECHNICAL NOTES:
+    - VS Code:
+        - The bpy object is "quantom entageled" with the blender file. We download blender-stubs instead to make the IDE [VS Code] get acces to autocomplete which makes code dev much easier in these modern times. This means, on the other hand, that the script will not be able to run from your IDE as it does not have acces to the blender file [only the stbs]. Run the script from blender instead:
+    - BLENDER:
+        - The base blender file [main.blend] is suppose to have a few 
+        - The Scripting window layout have been changed to be convenient for development with VS Code. After a change in VS Code a RED alert should be visible on the scripting tab. Click on it and 
+
+    - ENVIRONMENT:
+        - When you create your viritual envoronment to download the extra site-packages ensure it is the of the same version as whatever your blender is running:
+            - you can check the python version of blender by running the *python.exe* with the *--version flag* from the path similar to C:\Program Files\Blender Foundation\Blender 4.2\4.2\python\bin
--- script.py
+++ script.py
@@ -1,6 +1,12 @@
 ## BLENDER LIBS
-import bpy
-import bmesh
+BLENDER_AVAILABLE = False
+try:
+    import bpy
+    import bmesh
+    BLENDER_AVAILABLE = hasattr(bpy, "context")  # Check if bpy has actual Blender functionality
+except ImportError:
+    BLENDER_AVAILABLE = False
+
 import numpy as np
 
 # PYTHON CORE_LIBS
@@ -8,19 +14,21 @@ import sys
 import os
 import xml.etree.ElementTree as ET
 
-root = bpy.data.filepath        # The filepath of the .blend file you run the script in
-root = os.path.dirname(root)    # The repo you are working in [most likely]
-venv = '.venv'
+root = '.'
 
-pyhton_env_path = f'{root}\{venv}\Lib\site-packages'
-sys.path.append(pyhton_env_path)
+if BLENDER_AVAILABLE:
+    root = bpy.data.filepath        # The filepath of the .blend file you run the script in
+    root = os.path.dirname(root)    # The repo you are working in [most likely]
+    venv = 'venv'
+    pyhton_env_path = f'{root}\{venv}\Lib\site-packages'
+    sys.path.append(pyhton_env_path)
 
 # PYTHON SITE-PACKAGES
 import vtkmodules.all as vtk
-print(vtk.VTK_VERSION)
-
+print(f"vtk module version {vtk.VTK_VERSION}")
 
 def delete_from_collection(collection_name):
+    if not BLENDER_AVAILABLE: return
     """Deletes all objects in the specified collection except those named 'cube' or 'Cube'."""
     if collection_name in bpy.data.collections:
         collection = bpy.data.collections[collection_name]
@@ -28,30 +36,66 @@ def delete_from_collection(collection_name):
             if obj.name.lower() != 'cube':
                 bpy.data.objects.remove(obj, do_unlink=True)
 
-def extract_mesh_from_vtu(filepath, return_mesh=True):
-    # Create the VTK reader
-    reader = vtk.vtkXMLUnstructuredGridReader()
+def extract_NHFLOW_mesh_from_pvtu(filepath, with_attributes=True, return_mesh=True):
+    """
+    Reads a VTU/PVTU file and extracts the mesh as a BMesh (if in Blender) 
+    or a NumPy array of vertices (if outside Blender).
+    """
+    
+    # Check if the file is .pvtu (parallel) or .vtu (single file)
+    if filepath.endswith(".pvtu"):
+        reader = vtk.vtkXMLPUnstructuredGridReader()  # Use parallel reader
+    else:
+        reader = vtk.vtkXMLUnstructuredGridReader()  # Use single-file reader
     reader.SetFileName(filepath)
     reader.Update()
 
     # Get the mesh output from the reader
     mesh = reader.GetOutput()
     points = mesh.GetPoints()
-
+    
     # Create a new BMesh object
-    bm = bmesh.new()
+    if BLENDER_AVAILABLE:
+        bm = bmesh.new()
+        # Add custom layers for attributes
+        if with_attributes:
+            data_arrays = {
+                "velocity": mesh.GetPointData().GetArray(0),
+                "pressure": mesh.GetPointData().GetArray(1),
+                "omega_sig": mesh.GetPointData().GetArray(2),
+                "elevation": mesh.GetPointData().GetArray(3)
+            }
+            layer_velocity = bm.verts.layers.float_vector.new("velocity")
+            layer_pressure = bm.verts.layers.float.new("pressure")
+            layer_omega_sig = bm.verts.layers.float.new("omega_sig")
+            layer_elevation = bm.verts.layers.float.new("elevation")
+    else:
+        num_points = points.GetNumberOfPoints()  # Get the number of vertices
+        np_verts = np.zeros((num_points, 3), dtype=np.float32)  # Allocate space
 
     # Add vertices to BMesh
     if points:
         for x in range(points.GetNumberOfPoints()):
             a = [0, 0, 0]
             points.GetPoint(x, a)
-            bm.verts.new(a)
+
+            if BLENDER_AVAILABLE:
+                v = bm.verts.new(a)
+                if with_attributes:
+                    v[layer_velocity] = data_arrays["velocity"].GetTuple3(x)
+                    v[layer_pressure] = data_arrays["pressure"].GetValue(x)
+                    v[layer_omega_sig] = data_arrays["omega_sig"].GetValue(x)
+                    v[layer_elevation] = data_arrays["elevation"].GetValue(x)
+            else:
+                np_verts[x, :] = np.array(a, dtype=np.float32)  # Assign to NumPy array
 
     # Only return the vertecies if the mesh is not required
-    if not return_mesh:
-        return bm
-
+    if not return_mesh and BLENDER_AVAILABLE:
+        return bm      
+    if not BLENDER_AVAILABLE:
+        return np_verts 
+    
+    bm.verts.ensure_lookup_table()
     # Add faces to BMesh
     for i in range(mesh.GetNumberOfCells()):
         face = mesh.GetCell(i)
@@ -79,6 +123,7 @@ def fill_zeros(frame_count, length=8):
 #     return f'{zeros}{frame_count}'
 
 def create_water_object(mesh_data: bmesh, object_name: str, collection: str, visible_keyframe=-1):
+    if not BLENDER_AVAILABLE: return
     """Creates a Blender object with the given mesh data and assigns the 'Water' material."""
     # Create a new mesh and object
     mesh = bpy.data.meshes.new(object_name)
@@ -110,22 +155,22 @@ def create_water_object(mesh_data: bmesh, object_name: str, collection: str, vis
 
 if __name__ == "__main__":
     start_frame = 0
-    end_frame = 2  # 721
+    end_frame = 2  # TODO: change this to fit your simulation 721
 
     fps = 24
-    M10 = 1  # FROM THE REEF3D and DiveMESH Documentation
+    # M10 = 1  # FROM THE REEF3D and DiveMESH Documentation
 
+    filepath = "data/examples/single_split_experiment/REEF3D_NHFLOW_VTU" # TODO: change this
     file_name_skeleton_0 = "REEF3D-NHFLOW-"
-    file_name_skeleton_1 = "-00000001.vtu"
-    filepath_vtu = f"{root}/data/RUN1/REEF3D_NHFLOW_VTU/{file_name_skeleton_0}{fill_zeros(start_frame)}{file_name_skeleton_1}"
+    file_name_skeleton_1 = ".pvtu"
 
     collection = 'Waves'
-    print("MAIN-111111111111111111111")
     delete_from_collection(collection_name=collection)
     
-    print("MAIN-222222222222222222222")
     for frame in range(start_frame, end_frame):
-        print("MAIN-LOOOOOOOOOOOOOOOP")
-        filepath_vtu = f"{root}/data/RUN1/REEF3D_NHFLOW_VTU/{file_name_skeleton_0}{fill_zeros(frame)}{file_name_skeleton_1}"
-        mesh = extract_mesh_from_vtu(filepath_vtu)
+        filepath_pvtu = f"{root}/{filepath}/{file_name_skeleton_0}{fill_zeros(frame)}{file_name_skeleton_1}"
+        mesh = extract_NHFLOW_mesh_from_pvtu(filepath_pvtu, with_attributes=False, return_mesh=False)
         create_water_object(mesh, f'wave_{fill_zeros(frame, 3)}', collection, frame)
+
+        if not BLENDER_AVAILABLE:
+            print(mesh)
